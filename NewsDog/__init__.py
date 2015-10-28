a477beb656353e8bc982bb982ddcb9c71bbb1de4
Michael Findlater <michael@findlater.jp>
2015-10-28 20:05:01 +0900

    init

---
+++ NewsDog/__init__.py
@@ -0,0 +1,13 @@
+'''
+Created on 2015/10/28
+
+@author: michael
+'''
+
+__title__ = 'NewsDog'
+__version__ = '0.1'
+__author__ = 'Michael Findlater'
+__license__ = 'GNU V2'
+__copyright__ = 'Copyright 2015 Michael Findlater'
+
+from .newsdog import NewsDog
