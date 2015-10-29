4eedf4fb48c9433e5534a9447d1a3755916d67eb
Michael Findlater <michael@findlater.jp>
2015-10-29 14:30:49 +0900

    added fixes

--- NewsDog/redditor.py
+++ NewsDog/redditor.py
@@ -16,17 +16,13 @@ class Redditor():
         self.reddit = praw.Reddit(user_agent='NewsDog')
             
     def analyze_day(self, limit=5):
+        '''Returns a list of reddit links'''
+        ret = []
         submissions = self.reddit.get_subreddit(SUBREDDIT).get_top_from_day(limit=limit)
         for post in submissions:
             print(post)
             print(post.url)
             # Ignore links to reddit itself (against r/worldnews rules anyway)
             if 'www.reddit.com' not in post.url:
-                self.get_article(post.url)
-
-def main():
-    nd = NewsDog()
-    nd.analyze_day(limit=100)
-    nd.geo_csv()
-
-if __name__ == '__main__': main()
+                ret.append(post.url)
+        return ret
